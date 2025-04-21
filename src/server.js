b65cfe844d0e7a5e575906617a4a57b32f924256
Akash09082001 <aakashl@letsupgrade.in>
2025-04-21 17:51:25 +0530

    fix: issue fixed

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+import cluster from "cluster"
 import cors from "cors"
 import "dotenv/config"
 import express from "express"
 import morgan from "morgan"
+import { cpus } from "os"
 import { mongoConnect } from "./db/mongo.js"
 import authRouter from "./routes/v1/auth/auth.js"
 import dashboardRouter from "./routes/v1/client/dashboard.js"
@@ -17,26 +19,40 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Routes
-app.use("/v1/auth", authRouter)
-app.use("/v1/todos", clientRouter)
-app.use("/v1/todos/dashboard", dashboardRouter)
-app.use("/v1/user", userRouter)
+if (cluster.isPrimary) {
+	console.log(`Number of CPUs is ${cpus().length}`)
+	console.log(`Master ${process.pid} is running`)
 
-app.get("/", (req, res) => {
-	res.send("Hello World!")
-})
+	// Fork workers.
+	for (let i = 0; i < cpus().length; i++) {
+		cluster.fork()
+	}
 
-try {
+	cluster.on("exit", (worker, code, signal) => {
+		console.log(`worker ${worker.process.pid} died`)
+		console.log("Let's fork another worker!")
+		cluster.fork()
+	})
+} else {
+	app.use(express.json())
+	app.use(express.urlencoded({ extended: true }))
+
+	// Routes
+	app.use("/v1/auth", authRouter)
+	app.use("/v1/todos", clientRouter)
+	app.use("/v1/todos/dashboard", dashboardRouter)
+	app.use("/v1/user", userRouter)
+
+	app.get("/", (req, res) => {
+		res.send("Hello World!")
+	})
+
 	mongoConnect()
-	console.log("Database plugged in and healthy to serve.")
-} catch (error) {
-	console.error("Database connection error:", error)
-}
 
-if (process.env.NODE_ENV !== 'production') {
-	const PORT = process.env.PORT || 5000
-	app.listen(PORT, () => console.log(`Server ready on port ${PORT}`))
+	if (process.env.NODE_ENV !== "production") {
+		const PORT = process.env.PORT || 8000
+		app.listen(PORT, () => console.log(`Server ready on port ${PORT}`))
+	}
 }
 
 export default app
