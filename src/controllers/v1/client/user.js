7315bb133087495abe5785155b47b6f55d9b0748
Akash09082001 <aakashl@letsupgrade.in>
2025-05-05 18:18:14 +0530

    fix: simplify error response structure in auth, dashboard, todo, and user controllers

--- src/controllers/v1/client/user.js
+++ src/controllers/v1/client/user.js
@@ -1,64 +1,59 @@
-import { getProfileHelper, updateProfileHelper } from "../../../helper/v1/client/user.js"
+import {
+	getProfileHelper,
+	updateProfileHelper,
+} from "../../../helper/v1/client/user.js"
 
 const profile = async (req, res) => {
-    let uid = req.user.id
+	let uid = req.user.id
 
-    getProfileHelper(uid)
-        .then(async result => {
-            res.status(result.code).json({
-                message: result.message,
-                error: result.error,
-                code: result.code,
-                results: {
-                    data: result,
-                },
-            })
-        })
-        .catch(err => {
-            res.status(err?.code || 500).json({
-                message: "Error in getting user profile",
-                error: err.error || true,
-                code: err.code || 500,
-                results: {
-                    data: {
-                        error: err.message,
-                    },
-                },
-            })
-        })
+	getProfileHelper(uid)
+		.then(async result => {
+			res.status(result.code).json({
+				message: result.message,
+				error: result.error,
+				code: result.code,
+				results: {
+					data: result,
+				},
+			})
+		})
+		.catch(err => {
+			res.status(err?.code || 500).json({
+				message: "Error in getting user profile",
+				error: err.error || true,
+				code: err.code || 500,
+				results: {
+					data: err,
+				},
+			})
+		})
 }
 
 const update = async (req, res) => {
-    let uid = req.user.id
-    let body = req.body
+	let uid = req.user.id
+	let body = req.body
 
-    updateProfileHelper(uid, body)
-        .then(async result => {
-            res.status(result.code).json({
-                message: result.message,
-                error: result.error,
-                code: result.code,
-                results: {
-                    data: result,
-                },
-            })
-        })
-        .catch(err => {
-            res.status(err?.code || 500).json({
-                message: "Error in getting user profile",
-                error: err.error || true,
-                code: err.code || 500,
-                results: {
-                    data: {
-                        error: err.message,
-                    },
-                },
-            })
-        })
-
+	updateProfileHelper(uid, body)
+		.then(async result => {
+			res.status(result.code).json({
+				message: result.message,
+				error: result.error,
+				code: result.code,
+				results: {
+					data: result,
+				},
+			})
+		})
+		.catch(err => {
+			res.status(err?.code || 500).json({
+				message: "Error in getting user profile",
+				error: err.error || true,
+				code: err.code || 500,
+				results: {
+					data: err,
+				},
+			})
+		})
 }
 
-export {
-    profile,
-    update
-}
+export { profile, update }
