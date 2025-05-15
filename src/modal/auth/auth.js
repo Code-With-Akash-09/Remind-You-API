97e660c2d0c6b5158e35d8e5c221696532ed2be5
Akash09082001 <aakashl@letsupgrade.in>
2025-05-15 12:20:30 +0530

    fix: include description and designation fields in user profile retrieval and creation

--- src/modal/auth/auth.js
+++ src/modal/auth/auth.js
@@ -1,17 +1,18 @@
 import { general } from "../general.js"
 
 const addUser = async body => {
-    let generic = await general()
-    let data = {
-        ...generic,
-        isAdmin: false,
-        name: body?.name,
-        email: body?.email,
-        mobile: body?.mobile,
-        password: body?.password,
-    }
-    return data
+	let generic = await general()
+	let data = {
+		...generic,
+		isAdmin: false,
+		name: body?.name,
+		email: body?.email,
+		mobile: body?.mobile,
+		password: body?.password,
+		description: body?.description,
+		designation: body?.designation,
+	}
+	return data
 }
-
 
 export { addUser as addUserSchema }
