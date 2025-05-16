c5162f31160d0293285b8591ee8cd16097256323
Akash09082001 <aakashl@letsupgrade.in>
2025-05-16 17:28:16 +0530

    add: total count of todo api created

--- src/controllers/v1/client/dashboard.js
+++ src/controllers/v1/client/dashboard.js
@@ -1,4 +1,5 @@
 import {
+	getCountHelper,
 	getSearchHelper,
 	getTaskStateHelper,
 } from "../../../helper/v1/client/dashboard.js"
@@ -59,4 +60,30 @@ const search = async (req, res) => {
 		})
 }
 
-export { getTaskState, search }
+const getCount = async (req, res) => {
+	let uid = req.user.id
+
+	getCountHelper(uid)
+		.then(async result => {
+			res.status(result.code).json({
+				message: result.message,
+				error: result.error,
+				code: result.code,
+				results: {
+					data: result,
+				},
+			})
+		})
+		.catch(err => {
+			res.status(err?.code || 500).json({
+				message: "Error in getting count",
+				error: err.error || true,
+				code: err.code || 500,
+				results: {
+					data: err,
+				},
+			})
+		})
+}
+
+export { getCount, getTaskState, search }
